62422dd0aa2df7d2d4b76f4e3882609e49cd41cd
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-26 15:23:21 +0300

    setup signer middleware

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,13 @@
-use ethers::prelude::{Http, Provider};
+use ethers::{
+    prelude::{k256::ecdsa::SigningKey, Http, Provider, SignerMiddleware},
+    providers::Middleware,
+    signers::{LocalWallet, Signer, Wallet},
+};
+use std::sync::Arc;
 
 #[derive(Debug)]
 pub struct Config {
-    pub http: Provider<Http>,
+    pub http: Arc<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>,
     pub wss: String,
 }
 
@@ -11,10 +16,27 @@ impl Config {
         let netword_rpc = std::env::var("NETWORK_RPC").expect("missing NETWORK_RPC");
         let netword_wss = std::env::var("NETWORK_WSS").expect("missing NETWORK_WSS");
         let http = Provider::try_from(netword_rpc).unwrap();
+        let http = setup_signer(http).await;
+        let http = Arc::new(http);
 
         Config {
             http,
             wss: netword_wss,
         }
     }
+}
+
+async fn setup_signer(
+    provider: Provider<Http>,
+) -> SignerMiddleware<Provider<Http>, Wallet<SigningKey>> {
+    let chain_id = provider
+        .get_chainid()
+        .await
+        .expect("Failed to get chain id");
+    let private_key = std::env::var("PRIVATE_KEY").expect("Missing private key");
+    let wallet = private_key
+        .parse::<LocalWallet>()
+        .expect("Failed to parse wallet")
+        .with_chain_id(chain_id.as_u64());
+    SignerMiddleware::new(provider, wallet)
 }
