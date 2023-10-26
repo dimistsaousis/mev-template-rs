b2c745362ecf5da0b082ecd6efe5ad70f249675b
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-26 19:03:06 +0300

    run the program from main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
-use mev_template::config::Config;
-use mev_template::dex::Dex;
-use mev_template::helpers;
+use mev_template::run;
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
 
-    let config = Config::new().await;
-    let dex = Dex::new(
-        helpers::address(UNISWAP_FACTORY),
-        helpers::address(UNISWAP_ROUTER),
-        config,
-    );
-    println!("{:?}", dex.get_pairs().await);
+    run().await;
 }
